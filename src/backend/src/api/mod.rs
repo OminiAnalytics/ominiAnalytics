dd75e51f0be8acbab22140b18fd23f3af8808292
rea1bacon <victor.lefevre2004@gmail.com>
2022-08-28 00:06:08 +0200

    Added constants

--- src/backend/src/api/mod.rs
+++ src/backend/src/api/mod.rs
@@ -2,10 +2,11 @@
  * @Author: realbacon
  * @Date: 2022-08-26 11:34:38
  * @Last Modified by: realbacon
- * @Last Modified time: 2022-08-26 18:02:14
+ * @Last Modified time: 2022-08-26 23:39:45
  */
 pub mod alive_api;
 mod alive_db;
+mod constants;
 pub mod structs;
 pub mod uid_api;
 mod uid_db;
