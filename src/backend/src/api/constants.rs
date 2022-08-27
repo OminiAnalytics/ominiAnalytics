7e972d6514130dd46f7b5f7b08a3697dd111c443
rea1bacon <victor.lefevre2004@gmail.com>
2022-08-28 00:07:10 +0200

    Added constants

---
+++ src/backend/src/api/constants.rs
@@ -0,0 +1,3 @@
+pub const KILL_SESSION_DELAY: u8 = 30; // 30 seconds after last alive signal -> kill session
+pub const SIGNAL_TYPE_ALIVE: &str = "alive";
+pub const SIGNAL_TYPE_SESSION: &str = "session";
