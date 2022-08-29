843c798867b7f7d62d0a86236ce28c02f8120180
rea1bacon <victor.lefevre2004@gmail.com>
2022-08-29 11:11:30 +0200

    Chnaged main file name

--- src/jslib/webpack.config.js
+++ src/jslib/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./src/ominiTracker.js",
     target: "web",
     output: {
         path: path.resolve(__dirname, "dist"),
