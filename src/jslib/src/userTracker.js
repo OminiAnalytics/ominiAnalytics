1541c7c7b0b6b5d48dc2fd1b42db57ea89276d2f
rea1bacon <victor.lefevre2004@gmail.com>
2022-08-29 11:13:32 +0200

    Moved userHandler -> userTracker with only uid methods

---
+++ src/jslib/src/userTracker.js
@@ -0,0 +1,12 @@
+/*
+ * File: userTracker.js
+ * Created Date: 28 Aug 2022
+ * Author: realbacon
+ * -----
+ * Last Modified: 29/08/2022 11:06:56
+ * Modified By: realbacon
+ * -----
+ * Copyright (c) 2022 Omini
+ * -----
+ */
+
