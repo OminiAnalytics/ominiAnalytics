8b88de668cca7374b7c2c19ba0e69eb1ff657652
rea1bacon <victor.lefevre2004@gmail.com>
2022-08-29 11:24:09 +0200

    Os detection high compatibility using both UAdata and UA + device type detection high compatibility

--- src/jslib/src/deviceInfoTracker.js
+++ src/jslib/src/deviceInfoTracker.js
@@ -36,3 +36,74 @@
             version,
         };
     }
+
+    // @Experimental
+    // This function uses an experimental feature
+    // that is not yet supported in all browsers
+    // It fall back to getOsAndType() if not working
+    async getOsAndTypeExp() {
+        if (navigator.userAgentData === undefined) {
+            var os,
+                type = this.getOsAndType();
+            return [os, type];
+        }
+        var device_info = navigator.userAgentData.getHighEntropyValues([
+            "model",
+            "platform",
+            "platformVersion",
+            "fullVersionList",
+        ]);
+        var [os, type] = [{}, "unknown"];
+        await device_info.then((ua) => {
+            if (ua.platform === "Windows" && !ua.mobile) {
+                if (ua.platformVersion.split(".")[0] === "10") {
+                    os = {
+                        name: "Windows",
+                        version: "10",
+                    };
+                } else if (ua.platformVersion.split(".")[0] === "6") {
+                    os = {
+                        name: "Windows",
+                        version: "8.1 or lower",
+                    };
+                } else if (parseint(ua.platformVersion.split(".")[0]) >= 13) {
+                    os = {
+                        name: "Windows",
+                        version: "11",
+                    };
+                }
+                type = "desktop";
+            } else {
+                os = {
+                    name: ua.platform,
+                    version: ua.platformVersion,
+                };
+                type = ua.mobile ? "mobile" : "desktop";
+            }
+        });
+        return [os, type];
+    }
+
+    getOsAndType() {
+        // init ua
+        const ua = navigator.userAgent;
+        // Get device type
+        var type = new Boolean(false);
+        if ("maxTouchPoints" in navigator) {
+            type = navigator.maxTouchPoints > 0;
+        } else if ("msMaxTouchPoints" in navigator) {
+            type = navigator.msMaxTouchPoints > 0;
+        } else {
+            var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+            if (mQ && mQ.media === "(pointer:coarse)") {
+                type = !!mQ.matches;
+            } else if ("orientation" in window) {
+                type = true; // dépréciée mais utile au cas où
+            } else {
+                // en dernier recours, on regarde userAgent
+                var UA = navigator.userAgent;
+                type =
+                    /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+                    /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+            }
+        }
