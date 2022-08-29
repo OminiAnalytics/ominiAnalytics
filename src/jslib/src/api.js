e4a31ae3be3b73e18d0153286605a12cfe508cf1
rea1bacon <victor.lefevre2004@gmail.com>
2022-08-29 11:26:38 +0200

    Refactored ApiAsync class + created the Result object (inspired from rust)

--- src/jslib/src/api.js
+++ src/jslib/src/api.js
@@ -1,23 +1,35 @@
 /*
- * @Author: realbacon
- * @Date: 2022-08-25 11:31:58
- * @Last Modified by: realbacon
- * @Last Modified time: 2022-08-25 12:03:18
+ * File: api.js
+ * Created Date: 28 Aug 2022
+ * Author: realbacon
+ * -----
+ * Last Modified: 29/08/2022 11:05:53
+ * Modified By: realbacon
+ * -----
+ * Copyright (c) 2022 Omini
+ * -----
  */
 
 export class ApiAsync {
-    constructor(url, apiKey) {
+    constructor(url) {
+        // Delete the slash at the end of the url if it exists
         this.url = url;
-        this.key = apiKey;
+        if (this.url.slice(-1) === "/") {
+            this.url = this.url.slice(0, -1);
+        }
         this.endpoints = {
-            uid: "/uid",
-            parkour: "/parkour",
-            alive: "/alive",
+            uid: "/api/uid",
+            parkour: "/api/parkour",
+            alive: "/api/alive",
         };
     }
+    /*
+    endpoint : api endpoint @string
+    data : data to send @object
+    return : result @object
+    */
     async getData({ endpoint, params }) {
         var paramString = "";
-        params["key"] = this.key;
         paramString =
             "?" +
             Object.keys(params)
@@ -29,21 +41,25 @@ export class ApiAsync {
         const response = await fetch(
             this.url + this.endpoints[endpoint] + paramString,
             {
-                method: method || "GET",
+                method: "GET",
                 headers: {
                     "Content-Type": "application/json",
                 },
                 // allow cors
                 mode: "cors",
             }
-        );
-        const data = await response.json();
-        return data;
+        )
+            .then((response) => ["success", response.json()])
+            .catch((err) => {
+                console.error("Debug : " + err);
+                return ["error", err];
+            });
+        return response;
     }
 
     async postDataJSON({ endpoint, data }) {
-        data["key"] = this.key;
-        const response = await fetch(this.url + this.endpoints[endpoint], {
+        var Response = new Result();
+        await fetch(this.url + this.endpoints[endpoint], {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -51,8 +67,64 @@ export class ApiAsync {
             body: JSON.stringify(data),
             // allow cors
             mode: "cors",
-        });
-        const rdata = await response.json();
-        return rdata;
+        })
+            .then((resp) => {
+                if (resp.ok) {
+                    Response.new({
+                        result: "success",
+                        response: resp,
+                        error: resp.statusText,
+                    });
+                } else {
+                    Response.new({
+                        result: "error",
+                        response: resp,
+                        error: resp.statusText,
+                    });
+                    throw new Error(resp.statusText);
+                }
+            })
+            .catch((err) => {
+                console.error("Debug : " + err);
+                Response.new({
+                    result: "error",
+                    response: undefined,
+                    error: err,
+                });
+            });
+        return Response;
+    }
+}
+
+class Result {
+    constructor() {
+        this.result;
+        this.statusCode;
+        this.data;
+        this.headers;
+        this.error;
+    }
+
+    new({ result, response, error }) {
+        this.result = result;
+        this.statusCode = response.status;
+        this.headers = response.headers;
+        this.error = error;
+        this.data = response;
+    }
+
+    isErr() {
+        return this.result === "error";
+    }
+    isOk() {
+        return this.result === "success";
+    }
+
+    async toJson() {
+        this.data = await this.data.json().then((data) => data);
+    }
+
+    async clone() {
+        this.data = await this.data.clone().then((data) => data);
     }
 }
