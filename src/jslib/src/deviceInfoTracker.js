ecc5570cbd6d466472e21788d11e3e504a7c2bd8
rea1bacon <victor.lefevre2004@gmail.com>
2022-08-29 11:21:00 +0200

    Created deviceInfoTracker

---
+++ src/jslib/src/deviceInfoTracker.js
@@ -0,0 +1,11 @@
+/*
+ * File: deviceInfoTracker.js
+ * Created Date: 28 Aug 2022
+ * Author: realbacon
+ * -----
+ * Last Modified: 29/08/2022 02:30:59
+ * Modified By: realbacon
+ * -----
+ * Copyright (c) 2022 Omini
+ * -----
+ */
