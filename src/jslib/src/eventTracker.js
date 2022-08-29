6109d0b9e745bf9e0452108c44b387e93770dc0f
rea1bacon <victor.lefevre2004@gmail.com>
2022-08-29 11:20:17 +0200

    Created event tracker

---
+++ src/jslib/src/eventTracker.js
@@ -0,0 +1,14 @@
+/*
+ * File: eventTracker.js
+ * Created Date: 28 Aug 2022
+ * Author: realbacon
+ * -----
+ * Last Modified: 28/08/2022 10:03:59
+ * Modified By: realbacon
+ * -----
+ * Copyright (c) 2022 Omini
+ * -----
+*/
+
+
+
