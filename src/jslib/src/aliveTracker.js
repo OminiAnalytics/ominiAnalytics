0b8b7bb043deb5ecb8dcf13d4dfe87e0defc3782
rea1bacon <victor.lefevre2004@gmail.com>
2022-08-29 11:28:11 +0200

    Created aliveTracker

---
+++ src/jslib/src/aliveTracker.js
@@ -0,0 +1,11 @@
+/*
+ * File: alivesignal.js
+ * Created Date: Su Aug yyyy
+ * Author: realbacon
+ * -----
+ * Last Modified: 28/08/2022 10:04:26
+ * Modified By: realbacon
+ * -----
+ * Copyright (c) 2022 Omini
+ * -----
+ */
