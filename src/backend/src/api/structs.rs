eb0e7e96bd9ba707aa45f5ff21f7088c4a653afc
evilcater <victor.lefevre2004@gmail.com>
2022-08-30 22:19:19 +0200

    Changed /main api data structs

--- src/backend/src/api/structs.rs
+++ src/backend/src/api/structs.rs
@@ -1,59 +1,64 @@
 /*
  File: structs.rs
- Created Date: 28 Aug 2022
+ Created Date: 30 Aug 2022
  Author: realbacon
  -----
- Last Modified: 29/08/2022 02:32:6
+ Last Modified: 30/08/2022 09:55:50
  Modified By: realbacon
  -----
- Copyright (c) 2022 Omini
+ License  : MIT
  -----
 */
 
-use serde::{Deserialize, Serialize};
-use uuid::Uuid;
 
+use serde::{Deserialize, Serialize};
+
+/* @main */
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DeviceInfo {
-    pub browser: Browser,
-    pub os: Os,
-    pub dtype: String,
+    pub bro: Browser, // browser
+    pub os: Os,       // os
+    pub dty: String,  // device type
 }
 
+/* @main */
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Os {
-    pub name: String,
-    pub version: String,
+    pub nam: String, // name
+    pub ver: String, // version
 }
 
+/* @main */
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Browser {
-    pub name: String,
-    pub version: String,
+    pub nam: String, // name
+    pub ver: String, // version
 }
 
+/* @main */
 #[derive(Serialize, Deserialize, Debug)]
-pub struct UserResult {
+pub struct User {
     pub uid: String,
-    pub at: i64,
+    pub dev: DeviceInfo, // device
+    pub cou: String,     // country
 }
 
-#[derive(Deserialize, Serialize, Debug)]
-pub struct ValidUidMessage {
-    pub uid: String,
-    pub device: DeviceInfo,
-    pub country: String,
+/* @main */
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PageCall {
+    pub nam: String, // page name
+    pub url: String, // url
+}
+
+/* @main */
+#[derive(Serialize, Deserialize, Debug)]
+pub struct NewInst {
+    pub usr: User,     // user
+    pub pag: PageCall, // page
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct ValidAliveMessage {
-    pub u_id: String,
+    pub uid: String,
     pub date: i64,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct SignalResult {
-    pub success: bool,
-    pub message: String,
-    pub at: i64,
 }
