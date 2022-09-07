15894020222267cf3419191270e86b33a7b78969
rea1bacon <victor.lefevre2004@gmail.com>
2022-09-07 22:42:22 +0200

    header

--- src/jslib/src/eventTracker.js
+++ src/jslib/src/eventTracker.js
@@ -1,14 +1,11 @@
-/*
+/**
  * File: eventTracker.js
- * Created Date: 28 Aug 2022
+ * Created Date: 30 Aug 2022
  * Author: realbacon
  * -----
- * Last Modified: 28/08/2022 10:03:59
+ * Last Modified: 7/09/2022 07:46:21
  * Modified By: realbacon
  * -----
- * Copyright (c) 2022 Omini
+ * @license MIT
  * -----
-*/
-
-
-
+ **/
