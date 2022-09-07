45ab2b42eea0efe33f10678d901d9b6bfb52f008
rea1bacon <victor.lefevre2004@gmail.com>
2022-09-07 22:42:15 +0200

    Updated to eslint

--- src/jslib/src/api.js
+++ src/jslib/src/api.js
@@ -1,130 +1,152 @@
-/*
+/**
  * File: api.js
- * Created Date: 28 Aug 2022
+ * Created Date: 06 Sep 2022
  * Author: realbacon
  * -----
- * Last Modified: 29/08/2022 11:05:53
+ * Last Modified: 7/09/2022 07:48:19
  * Modified By: realbacon
  * -----
- * Copyright (c) 2022 Omini
+ * @license MIT
  * -----
- */
+ **/
 
 export class ApiAsync {
-    constructor(url) {
-        // Delete the slash at the end of the url if it exists
-        this.url = url;
-        if (this.url.slice(-1) === "/") {
-            this.url = this.url.slice(0, -1);
-        }
-        this.endpoints = {
-            uid: "/api/uid",
-            parkour: "/api/parkour",
-            alive: "/api/alive",
-        };
+  constructor (url) {
+    // Delete the slash at the end of the url if it exists
+    this.url = url
+    if (this.url.slice(-1) === '/') {
+      this.url = this.url.slice(0, -1)
     }
-    /*
+    this.endpoints = {
+      uid: '/api/uid',
+      parkour: '/api/parkour',
+      alive: '/api/alive',
+      key: '/api/key'
+    }
+  }
+
+  /*
     endpoint : api endpoint @string
     data : data to send @object
     return : result @object
     */
-    async getData({ endpoint, params }) {
-        var paramString = "";
-        paramString =
-            "?" +
-            Object.keys(params)
-                .map(function (key) {
-                    return key + "=" + params[key];
-                })
-                .join("&");
-
-        const response = await fetch(
-            this.url + this.endpoints[endpoint] + paramString,
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                // allow cors
-                mode: "cors",
-            }
-        )
-            .then((response) => ["success", response.json()])
-            .catch((err) => {
-                console.error("Debug : " + err);
-                return ["error", err];
-            });
-        return response;
-    }
-
-    async postDataJSON({ endpoint, data }) {
-        var Response = new Result();
-        await fetch(this.url + this.endpoints[endpoint], {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-            // allow cors
-            mode: "cors",
+  async getData ({ endpoint, params, headers }) {
+    const Response = new Result()
+    let paramString = ''
+    paramString =
+      '?' +
+      Object.keys(params)
+        .map(function (key) {
+          return key + '=' + params[key]
         })
-            .then((resp) => {
-                if (resp.ok) {
-                    Response.new({
-                        result: "success",
-                        response: resp,
-                        error: resp.statusText,
-                    });
-                } else {
-                    Response.new({
-                        result: "error",
-                        response: resp,
-                        error: resp.statusText,
-                    });
-                    throw new Error(resp.statusText);
-                }
-            })
-            .catch((err) => {
-                console.error("Debug : " + err);
-                Response.new({
-                    result: "error",
-                    response: undefined,
-                    error: err,
-                });
-            });
-        return Response;
-    }
+        .join('&')
+    headers['Content-Type'] = 'application/json'
+    // eslint-disable-next-line no-unused-vars
+    const _response = await fetch(
+      this.url + this.endpoints[endpoint] + paramString,
+      {
+        method: 'GET',
+        headers,
+        // allow cors
+        mode: 'cors'
+      }
+    )
+      .then((resp) => {
+        if (resp.ok) {
+          Response.new({
+            result: 'success',
+            response: resp,
+            error: resp.statusText
+          })
+        } else {
+          Response.new({
+            result: 'error',
+            response: resp,
+            error: resp.statusText
+          })
+          throw new Error(resp.statusText)
+        }
+      })
+      .catch((err) => {
+        console.error('Debug : ' + err)
+        Response.new({
+          result: 'error',
+          response: undefined,
+          error: err
+        })
+      })
+    return Response
+  }
+
+  async postDataJSON ({ endpoint, data }) {
+    const Response = new Result()
+    await fetch(this.url + this.endpoints[endpoint], {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+      // allow cors
+      mode: 'cors'
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          Response.new({
+            result: 'success',
+            response: resp,
+            error: resp.statusText
+          })
+        } else {
+          Response.new({
+            result: 'error',
+            response: resp,
+            error: resp.statusText
+          })
+          throw new Error(resp.statusText)
+        }
+      })
+      .catch((err) => {
+        console.error('Debug : ' + err)
+        Response.new({
+          result: 'error',
+          response: undefined,
+          error: err
+        })
+      })
+    return Response
+  }
 }
 
 class Result {
-    constructor() {
-        this.result;
-        this.statusCode;
-        this.data;
-        this.headers;
-        this.error;
-    }
+  constructor () {
+    this.result = undefined
+    this.statusCode = undefined
+    this.data = undefined
+    this.headers = undefined
+    this.error = undefined
+  }
 
-    new({ result, response, error }) {
-        this.result = result;
-        this.statusCode = response.status;
-        this.headers = response.headers;
-        this.error = error;
-        this.data = response;
-    }
+  new ({ result, response, error }) {
+    this.result = result
+    this.statusCode = response.status
+    this.headers = response.headers
+    this.error = error
+    this.data = response
+  }
 
-    isErr() {
-        return this.result === "error";
-    }
-    isOk() {
-        return this.result === "success";
-    }
+  isErr () {
+    return this.result === 'error'
+  }
 
-    async toJson() {
-        this.data = await this.data.json().then((data) => data);
-    }
+  isOk () {
+    return this.result === 'success'
+  }
 
-    async clone() {
-        this.data = await this.data.clone().then((data) => data);
-    }
+  async toJson () {
+    this.data = await this.data.json().then((data) => data)
+  }
+
+  async clone () {
+    this.data = await this.data.clone().then((data) => data)
+  }
 }
