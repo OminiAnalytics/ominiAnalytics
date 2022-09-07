4a9fcdb9163fbf0784e4fdbc06f612f62cc35b9c
rea1bacon <victor.lefevre2004@gmail.com>
2022-09-07 22:48:18 +0200

    declare error module

--- src/backend/src/api/mod.rs
+++ src/backend/src/api/mod.rs
@@ -3,16 +3,16 @@
  Created Date: 25 Aug 2022
  Author: realbacon
  -----
- Last Modified: 30/08/2022 09:55:29
+ Last Modified: 31/08/2022 02:11:29
  Modified By: realbacon
  -----
  License  : MIT
  -----
 */
 
-
 pub mod alive;
 mod constants;
+pub mod errors;
 pub mod main;
 pub mod security;
 pub mod structs;
