2d252d025c3b4d6c3707ec918020e140ac16f755
rea1bacon <victor.lefevre2004@gmail.com>
2022-09-10 00:03:45 +0200

    added crypt o and device

--- src/backend/src/api/main/mod.rs
+++ src/backend/src/api/main/mod.rs
@@ -3,13 +3,14 @@
  Created Date: 30 Aug 2022
  Author: realbacon
  -----
- Last Modified: 30/08/2022 09:55:7
+ Last Modified: 8/09/2022 09:03:48
  Modified By: realbacon
  -----
  License  : MIT
  -----
 */
 
-
+pub mod crypto;
 pub mod db;
+pub mod device;
 pub mod endpoint;
