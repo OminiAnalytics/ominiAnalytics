99417631827b2eafa023d858de18de8c143fd587
rea1bacon <victor.lefevre2004@gmail.com>
2022-09-13 22:37:09 +0200

    Updated model

--- src/backend/src/models.rs
+++ src/backend/src/models.rs
@@ -3,14 +3,13 @@
  Created Date: 30 Aug 2022
  Author: realbacon
  -----
- Last Modified: 31/08/2022 07:42:25
+ Last Modified: 13/09/2022 10:36:56
  Modified By: realbacon
  -----
  License  : MIT
  -----
 */
 
-use serde::{Deserialize, Serialize};
 use tokio_pg_mapper_derive::PostgresMapper;
 use uuid::Uuid;
 
@@ -22,24 +21,4 @@ pub struct NewAliveMessage {
     pub s_id: Uuid,
     pub mtype: String,
     pub s_duration: i32,
-}
-
-#[derive(PostgresMapper, Debug, Serialize, Deserialize)]
-#[pg_mapper(table = "omini_users")]
-pub struct GetUser {
-    pub id: Uuid,
-    pub created_at: i64,
-    pub updated_at: i64,
-    pub device_info: serde_json::Value,
-    pub country: String,
-    pub ip: String,
-}
-
-#[derive(PostgresMapper, Debug, Serialize, Deserialize)]
-#[pg_mapper(table = "omini_users")]
-pub struct NewUser {
-    pub id: Uuid,
-    pub device_info: serde_json::Value,
-    pub country: String,
-    pub ip: String,
 }
