a2ded0c354003057a6bb586b3bb128af588456e4
rea1bacon <victor.lefevre2004@gmail.com>
2022-09-13 22:37:33 +0200

    alive -> event

--- src/backend/src/api/mod.rs
+++ src/backend/src/api/mod.rs
@@ -3,14 +3,14 @@
  Created Date: 25 Aug 2022
  Author: realbacon
  -----
- Last Modified: 8/09/2022 05:34:12
+ Last Modified: 13/09/2022 08:04:20
  Modified By: realbacon
  -----
  License  : MIT
  -----
 */
 
-pub mod alive;
 pub mod custom;
+pub mod event;
 pub mod main;
 pub mod security;
