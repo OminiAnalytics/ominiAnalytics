578158ec9c619fec536ea9ffb88379c5dad6673e
rea1bacon <victor.lefevre2004@gmail.com>
2022-09-13 22:34:23 +0200

    Created mod.rs

---
+++ src/backend/src/api/event/mod.rs
@@ -0,0 +1,16 @@
+/*
+ File: mod.rs
+ Created Date: 30 Aug 2022
+ Author: realbacon
+ -----
+ Last Modified: 13/09/2022 09:24:46
+ Modified By: realbacon
+ -----
+ License  : MIT
+ -----
+*/
+
+pub mod alive;
+pub mod handlers;
+pub mod sql;
+pub mod structs;
